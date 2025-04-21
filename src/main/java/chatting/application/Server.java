2dfa586b2d0cbc31ce7ae34da5d17e3a5ab63a3c
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-21 12:52:36 +0600

    back image icon added to the server side

--- src/main/java/chatting/application/Server.java
+++ src/main/java/chatting/application/Server.java
@@ -5,16 +5,25 @@ import java.awt.*;
 
 public class Server extends JFrame {
     private JPanel headerPanel;
+    private ImageIcon backIcon,backIconMain;
+    private Image backImage;
+    private JLabel backlabel;
 
     Server(){
         setLayout(null);
         headerPanel=new JPanel();
         headerPanel.setBackground(new Color(7,94,84));
-
-
         headerPanel.setBounds(0,0,450,70);
-        add(headerPanel);
+        headerPanel.setLayout(null);
 
+        backIcon = new ImageIcon(getClass().getResource("/icons/3.png"));
+        backImage=backIcon.getImage().getScaledInstance(25,25,Image.SCALE_DEFAULT);
+        backIconMain=new ImageIcon(backImage);
+        backlabel=new JLabel(backIconMain);
+        backlabel.setBounds(5, 20, 25, 25);
+        headerPanel.add(backlabel);
+
+        add(headerPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(450,700);
         getContentPane().setBackground(Color.WHITE);
