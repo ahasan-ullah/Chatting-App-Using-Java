624ca4bedec59cea4a2ebc81245d50e2cee490bb
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-21 12:17:19 +0600

    header panel added to also icons folder added

--- src/main/java/chatting/application/Server.java
+++ src/main/java/chatting/application/Server.java
@@ -4,10 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Server extends JFrame {
+    private JPanel headerPanel;
 
     Server(){
         setLayout(null);
+        headerPanel=new JPanel();
+        headerPanel.setBackground(new Color(7,94,84));
 
+
+        headerPanel.setBounds(0,0,450,70);
+        add(headerPanel);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(450,700);
         getContentPane().setBackground(Color.WHITE);
         setLocation(200,50);
