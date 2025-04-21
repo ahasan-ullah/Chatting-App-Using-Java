5039ae5df0af66088a8e676cbb128dd05e18afe5
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-21 11:51:43 +0600

    basic frame designed for server side

--- src/main/java/chatting/application/Server.java
+++ src/main/java/chatting/application/Server.java
@@ -1,4 +1,20 @@
 package chatting.application;
 
-public class Server {
+import javax.swing.*;
+import java.awt.*;
+
+public class Server extends JFrame {
+
+    Server(){
+        setLayout(null);
+
+        setSize(450,700);
+        getContentPane().setBackground(Color.WHITE);
+        setLocation(200,50);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Server();
+    }
 }
